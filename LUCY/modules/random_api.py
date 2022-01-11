d023cd6331de9539a68c0be19334ecc9557b527a
RISHABH ANANAD <93331538+mafiarishabh@users.noreply.github.com>
2022-01-11 11:30:14 +0530

    Update random_api.py

--- LUCY/modules/random_api.py
+++ LUCY/modules/random_api.py
@@ -46,7 +46,7 @@ import urllib
 import asyncio
 import os
 from pyrogram import filters
-from Zaid import TEMP_DOWNLOAD_DIRECTORY, pbot
+from LUCY import TEMP_DOWNLOAD_DIRECTORY, pbot
 
 
 @pbot.on_message(filters.command("boobs"))
