04cf49f208e3d3e65911f7d0b96dcab10a656e13
RISHABH ANANAD <93331538+mafiarishabh@users.noreply.github.com>
2022-01-11 09:47:14 +0530

    Add files via upload

---
+++ LUCY/mongo.py
@@ -0,0 +1,22 @@
+import asyncio
+import sys
+from motor import motor_asyncio
+from pymongo import MongoClient
+from pymongo.errors import ServerSelectionTimeoutError
+from LUCY import MONGO_DB_URI
+
+
+MONGO_PORT = "27017"
+MONGO_DB_URI = "mongodb+srv://lucyx:rishabh123classmate@cluster0.hx2qi.mongodb.net/lucyx?retryWrites=true&w=majority"
+MONGO_DB = "ZaidRobot"
+
+
+client = MongoClient()
+client = MongoClient(MONGO_DB_URI, 27017)[MONGO_DB]
+motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, 27017)
+db = motor[MONGO_DB]
+db = client["ZaidRobot"]
+try:
+    asyncio.get_event_loop().run_until_complete(motor.server_info())
+except ServerSelectionTimeoutError:
+    sys.exit(log.critical("Can't connect to mongodb! Exiting..."))
